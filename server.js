56299c597cfb535a65e78ba06fd01b4453c3e2b0
Le0finz <leo.fssmiranda@gmail.com>
2022-12-04 16:43:40 -0300

    Configuração de Login/Registro

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const route = require("./src/routes/route")
 require("dotenv").config();
+const path = require("path")
 
+app.set('view engine', 'ejs');
+app.set("views",path.resolve(__dirname , "src","views"));
 app.use(route)
 app.listen(process.env.PORT, () => {
     console.log("listening on http://localhost:",process.env.PORT);
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const route = express.Router()
-route.get('/', (req, res) => {res.send("Hello! world");});
+
+route.get('/', (req, res) => {res.render("landingPage");});
+route.get('/login', (req, res) => {res.render("login")});
+route.get('/register', (req, res) => {res.render("register")});
 
 module.exports = route
