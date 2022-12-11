7f2a535f9ac4a3c68e9f7340147aaa1badba60b6
Le0finz <leo.fssmiranda@gmail.com>
2022-12-11 18:56:38 -0300

    criando homepage user/admin

--- server.js
+++ server.js
@@ -3,6 +3,23 @@ const app = express();
 const route = require("./src/routes/route")
 require("dotenv").config();
 const path = require("path")
+const handlebars = require("express-handlebars");
+
+const Sequelize =  require("Sequelize");
+const sequelize = new Sequelize("ruptura", "root", "Leonardo123@",{
+    host : "localhost",
+    dialect : "mysql"
+});
+
+sequelize.authenticate().then (function(){
+    console.log("Authenticated")
+}).catch (function(err){
+    console.log("Failed to authenticate: " + err.message)
+})
+
+//handlebars
+app.engine("handlebars", handlebars({defaultLayout: "main"}))
+app.set("view engine", "handlebars")
 
 app.set('view engine', 'ejs');
 app.set("views",path.resolve(__dirname , "src","views"));
--- src/routes/route.js
+++ src/routes/route.js
@@ -4,5 +4,7 @@ const route = express.Router()
 route.get('/', (req, res) => {res.render("landingPage");});
 route.get('/login', (req, res) => {res.render("login")});
 route.get('/register', (req, res) => {res.render("register")});
+route.post('/userHomepage', (req, res) => {res.render("userHomepage")});
+route.post('/adminHomepage', (req, res) => {res.render("adminHomepage")});
 
 module.exports = route
